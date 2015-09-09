9bf138381f9550fea8a01d510111c6ff91de59cc
Lord Bharath Bhushan Lohray <bharath@lohray.com>
2015-09-09 05:06:44 -0700

    Added an indexed addressing game play.

--- test2.py
+++ test2.py
@@ -1,46 +1,46 @@
 #! /usr/bin/python3
 from xo import xo
 
-# Test for X Y addressing game
+# Test for inedxed addressing game
 
 def main():
 	print("Tic Tac Toe Platform")
 	g=xo()
 	print(g.getBoard())
 	
-	print('Play :'+repr(g.set_X(0,0)))
+	print('Play :'+repr(g.set_absolute(0,2)))
 	print(g.getBoard())
 	print('----------')
 	
-	print('Play :'+repr(g.set_O(0,1)))
+	print('Play :'+repr(g.set_absolute(1,1)))
 	print(g.getBoard())
 	print('----------')
 	
-	print('Play :'+str(g.set_X(0,2)))
+	print('Play :'+str(g.set_absolute(2,2)))
 	print(g.getBoard())
 	print('----------')
 	
-	print('Play :'+str(g.set_O(1,0)))
+	print('Play :'+str(g.set_absolute(3,1)))
 	print(g.getBoard())
 	print('----------')
 
-	print('Play :'+str(g.set_X(1,1)))
+	print('Play :'+str(g.set_absolute(4,2)))
 	print(g.getBoard())
 	print('----------')
 	
-	print('Play :'+str(g.set_O(1,2)))
+	print('Play :'+str(g.set_absolute(5,1)))
 	print(g.getBoard())
 	print('----------')
 	
-	print('Play :'+str(g.set_X(2,0)))
+	print('Play :'+str(g.set_absolute(6,2)))
 	print(g.getBoard())
 	print('----------')
 	
-	print('Play :'+str(g.set_O(2,1)))
+	print('Play :'+str(g.set_absolute(7,1)))
 	print(g.getBoard())
 	print('----------')
 	
-	print('Play :'+str(g.set_X(2,2)))
+	print('Play :'+str(g.set_absolute(8,2)))
 	print(g.getBoard())
 	print('----------')
 
--- xo.py
+++ xo.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 from enum import Enum
+import math
 
 class res(Enum):
 	OK = 0
@@ -28,45 +29,94 @@ class xo:
 			self.board[pos_x][pos_y]=player
 		return 0
 		
-	def set_X(self, pos_x, pos_y):
-		# check if X is playing first.
-		if self.turn == 0:
-			self.X_TURN_MODULUS = 0
-			self.O_TURN_MODULUS = 1
-		# check if X is not playing out of turn.
-		if self.turn % 2 == self.X_TURN_MODULUS:
-			# check if we are overwriting a position
-			if (self.board[pos_x][pos_y] == 0):
-				self.board[pos_x][pos_y] = self.PLAYER_X
-				self.turn += 1
-				# Check if the last move resulted in a victory
-				return self.has_won(self.PLAYER_X)
-			else:
-				return self.res.INVALID_MOVE
+	def set_absolute(self,pos,player):
+		if (self.turn == 0):
+			if (player == 1):
+				self.X_TURN_MODULUS = 1
+				self.O_TURN_MODULUS = 0
+			elif (player == 2):
+				self.X_TURN_MODULUS = 0
+				self.O_TURN_MODULUS = 1
+		if ( self.game_over == True ):
+			returnVal=res.INVALID_MOVE
 		else:
-			return self.res.OUT_OF_TURN
+			pos_x = math.floor( pos / 3 )
+			pos_y = pos % 3
+			if (player == 1):
+				# if player 1, Check if player is playing out of turn.
+				if (self.turn % 2 == self.O_TURN_MODULUS):
+					# check if we are overwriting a position
+					if (self.board[pos_x][pos_y] == 0):
+						self.board[pos_x][pos_y] = self.PLAYER_O
+						self.turn += 1
+						returnVal = self.has_won(self.PLAYER_O)
+					else:
+						returnVal = self.res.INVALID_MOVE;	
+				else:
+					returnVal = self.res.OUT_OF_TURN
+			elif (player == 2):
+				# if player 2, Check if player is playing out of turn.
+				if (self.turn % 2 == self.X_TURN_MODULUS):
+					# check if we are overwriting a position
+					if (self.board[pos_x][pos_y] == 0):
+						self.board[pos_x][pos_y] = self.PLAYER_X
+						self.turn += 1
+						returnVal = self.has_won(self.PLAYER_X)
+					else:
+						returnVal = self.res.INVALID_MOVE;
+				else:
+					returnVal = self.res.OUT_OF_TURN
+			else:
+				# actually invalid player
+				returnVal = self.res.INVALID_MOVE
+		return returnVal
+		
+	def set_X(self, pos_x, pos_y):
+		if ( self.game_over == False ):
+			# check if X is playing first.
+			if self.turn == 0:
+				self.X_TURN_MODULUS = 0
+				self.O_TURN_MODULUS = 1
+			# check if X is not playing out of turn.
+			if self.turn % 2 == self.X_TURN_MODULUS:
+				# check if we are overwriting a position
+				if (self.board[pos_x][pos_y] == 0):
+					self.board[pos_x][pos_y] = self.PLAYER_X
+					self.turn += 1
+					# Check if the last move resulted in a victory
+					return self.has_won(self.PLAYER_X)
+				else:
+					return self.res.INVALID_MOVE
+			else:
+				return self.res.OUT_OF_TURN
+		else:
+			return self.res.INVALID_MOVE;
 	
 	def set_O(self,pos_x,pos_y):
 		# check if O is playing first.
-		if self.turn == 0:
-			self.X_TURN_MODULUS = 1
-			self.O_TURN_MODULUS = 0
-		# check if O is not playing out of turn.
-		if self.turn % 2 == self.O_TURN_MODULUS:
-			# check if we are overwriting a position
-			if ( self.board[pos_x][pos_y] == 0 ):
-				self.board[pos_x][pos_y] = self.PLAYER_O
-				self.turn += 1
-				# Check if the last move resulted in a victory
-				return self.has_won(self.PLAYER_O)
+		if ( self.game_over == False ):
+			if self.turn == 0:
+				self.X_TURN_MODULUS = 1
+				self.O_TURN_MODULUS = 0
+			# check if O is not playing out of turn.
+			if self.turn % 2 == self.O_TURN_MODULUS:
+				# check if we are overwriting a position
+				if ( self.board[pos_x][pos_y] == 0 ):
+					self.board[pos_x][pos_y] = self.PLAYER_O
+					self.turn += 1
+					# Check if the last move resulted in a victory
+					return self.has_won(self.PLAYER_O)
+				else:
+					return self.res.INVALID_MOVE
 			else:
-				return self.res.INVALID_MOVE
+				return self.res.OUT_OF_TURN
 		else:
-			return self.res.OUT_OF_TURN
+			return self.res.INVALID_MOVE
 	
 	def has_won(self,player):
 		self.count_empty_squares();
-		transBoard = list(map(list, zip(*self.board)))
+		#transBoard = list(map(list, zip(*self.board)))
+		transBoard = list(zip(*self.board[::-1]))
 		result = self.check_board_for_winner( self.board, player ) | self.check_board_for_winner( transBoard, player )
 		returnVal = self.res.OK
 		# check for draw
@@ -107,7 +157,16 @@ class xo:
 		if ( [board[0][0], board[1][1], board[2][2]] == [ player, player, player ] ):
 			win = True
 		return win
-		
+	
+	def transpose(self,board):
+		c=0;
+		for b in board:
+			r=0
+			for e in b:
+				transBoard[c][r]=e;
+				r += 1
+			c += 1
+				
 	
 	def getBoard(self):
 		boardString="";
@@ -155,7 +214,7 @@ def main():
 	print('----------')
 	
 	# invalid move
-	print('Play :'+str(g.set_X(0,1)))
+	print('Play :'+str(g.set_O(0,1)))
 	print(g.getBoard())
 	print('----------')
 	
