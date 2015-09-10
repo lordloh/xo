6a281cf3a733d3c188ae0a627e2160e20e468e32
Lord Bharath Bhushan Lohray <bharath@lohray.com>
2015-09-10 02:38:16 -0700

    A human player class * and a test function for human vs human

---
+++ human.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+class human:
+    def __init__(self,play_as):
+        self.name = input("Enter your name :")
+        self.sym = [ ' ', '0', 'X' ]
+        self.play_as = play_as;
+        
+    def play(self,board):
+        boardString = "";
+        for i in range(0, 3):
+            for j in range (0,3):
+                if j < 2:
+                	boardString += self.sym[board[i][j]] + '| ';
+                else:
+                	boardString += self.sym[board[i][j]];
+            boardString += "\n"
+        print (boardString,end="")
+        move = input(self.name+", enter the square number you want to mark with '"+self.sym[self.play_as]+"' (0-8) :")
+        return int(move)
+        
+def main():
+    player=human(1);
+    player.play([[0,0,0],[0,0,0],[0,0,0]])
+
+if __name__ == '__main__':
+	main()    
