4a10ea394a6061856b885045b80dfe0f73ff8e80
Lord Bharath Bhushan Lohray <bharath@lohray.com>
2015-09-20 01:27:46 -0700

    attempted to format the code to PEP8 specification

--- random1SPlayer.py
+++ random1SPlayer.py
@@ -6,63 +6,64 @@ from err import err
 from copy import *
 from numpy import *
 
+
 class random1SPlayer:
     """
-    The random player with 1 step look ahead finds a list of valid moves, and 
-    chooses one among it such that it can win the next move, or prevent an 
+    The random player with 1 step look ahead finds a list of valid moves, and
+    chooses one among it such that it can win the next move, or prevent an
     adversary from winning in the next move.
-    
+
     Essentially this is a 1 level min max algorithm.
-    
     """
-    def __init__(o,play_as,name,game):
-        o.play_as = play_as;
+    def __init__(o, play_as, name, game):
+        o.play_as = play_as
         o.name = name
         o.title = "Random Player with one step look ahead"
         o.game = game
-        
+
     def play(o):
         # Compute possible moves
-        possible_moves=[];
+        possible_move = []
         n = 0
         board = deepcopy(o.game.brd.board)
         for b in board:
             for e in b:
                 if (e == 0):
-                    possible_moves=possible_moves+[n]
+                    possible_moves = possible_moves + [n]
                 n += 1
         move = -1
         # For each of the listed move, see if the player can win in the next move
         for m in possible_moves:
             new_board = deepcopy(board)
-            x,y=o.game.brd.get_xy_from_pos(m)
-            new_board[x][y]=o.play_as;
-            result = o.game.stateless_has_won(new_board,o.play_as)
+            x, y = o.game.brd.get_xy_from_pos(m)
+            new_board[x][y] = o.play_as
+            result = o.game.stateless_has_won(new_board, o.play_as)
             if (result == err.WIN):
-                move = m;
+                move = m
                 break
         # We cannot win in the next move.
         if (move == -1):
             # See if our adversary can win if give a chance to play.
             # Compute the adversary's id
-            temp = roll(o.game.play_order,1)
-            adversary=temp[o.play_as-1]
+            temp = roll(o.game.play_order, 1)
+            adversary = temp[o.play_as - 1]
             for m in possible_moves:
                 new_board = deepcopy(board)
-                x,y=o.game.brd.get_xy_from_pos(m)
-                new_board[x][y]=adversary;
-                result = o.game.stateless_has_won(new_board,adversary)
+                x, y = o.game.brd.get_xy_from_pos(m)
+                new_board[x][y] = adversary
+                result = o.game.stateless_has_won(new_board, adversary)
                 if (result == err.WIN):
-                    move = m;
+                    move = m
                     break
         if (move == -1):
             move = possible_moves[randint(0, len(possible_moves)-1)]
         else:
             move = m
         return int(move)
-        
+
+
 def main():
     pass
 
 if __name__ == '__main__':
-	main()    
+	main()
